ebc7a7031fe369ea244ff9f7aadf3d7e556777c0
Fabio Vinicius <fabiovinicius1@outlook.com>
2024-07-30 14:40:21 -0300

    feat: criacao empresaRepository

---
+++ src/repositories/empresaRepository.ts
@@ -0,0 +1,41 @@
+import { Empresa } from '../models/empresaModel';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export const pesquisarEmpresaPeloIdRepository = async (id: number): Promise<Empresa | null> => {
+	const resultEmpresa = await prisma.empresas.findFirst({
+		where:{
+			id
+		}
+	})
+  return resultEmpresa;
+};
+
+export const adicionarEmpresaRepository = async (empresa: Empresa): Promise<Empresa | null> => {
+	const {id,nome,endereco,descricao,contato,cnpj,email} = empresa;
+	const resultEmpresa = await prisma.empresas.create({
+		data:{
+			id,
+			nome,
+			endereco,
+			descricao,
+			contato,
+			cnpj,
+			email
+		}
+	})
+ return resultEmpresa;
+};
+
+export const atualizarContatoEmpresaService = async (id: number, contato: string): Promise<Empresa | null> => {
+	const resultEmpresa = await prisma.empresas.update({
+		where:{
+			id
+		},
+		data: {
+			contato
+		}
+	})
+  return resultEmpresa;
+};
