09de27bb4aee6edad174b3bf56d4a34c22af26fc
Fabio Vinicius <fabiovinicius1@outlook.com>
2024-07-30 15:01:16 -0300

    feat: adição empresaController

---
+++ src/controllers/empresasController.ts
@@ -0,0 +1,40 @@
+import { Request, Response, Router } from 'express';
+import { Empresa } from '../models/empresaModel';
+import { pesquisarEmpresaService } from '../services/pesquisarEmpresaService';
+import { atualizarContatoEmpresaService } from '../services/atualizarContatoEmpresaService';
+import { adicionarEmpresaService } from '../services/adicionarEmpresaService';
+
+const router = Router();
+
+router.get('/pesquisar/empresa', async (req: Request, res: Response) => {
+  const idEmpresa = req.body.id;
+  const result = await pesquisarEmpresaService(idEmpresa);
+  if (result) {
+    return res.status(200).json(result);
+  } else {
+    return res.status(404).json({ message: "Empresa não encontrada" });
+  }
+});
+
+router.post('/adicionar/empresa', async (req: Request, res: Response) => {
+  const empresa:Empresa = req.body;
+  const result = await adicionarEmpresaService(empresa);	
+  if (result) {
+    return res.status(201).json(result);
+  } else {
+    return res.status(404).json({ message: "Empresa com cnpj já existente" });
+  }
+});
+
+router.patch('/atualizar/empresa/contato', async (req: Request, res: Response) => {
+  const idEmpresa = req.body.id;
+  const contato = req.body.contato;
+  const result = await atualizarContatoEmpresaService(idEmpresa,contato);
+  if (result) {
+    return res.json(result);
+  } else {
+    return res.status(404).json({ message: 'Empresa não encontrada' });
+  }
+});
+
+export default router;
