dbb2d49940d84ba2b3901fa0730d1f4e48aa180f
Fabio Vinicius <fabiovinicius1@outlook.com>
2024-09-06 21:14:45 -0300

    aidçao jest

---
+++ tests/empresa.spec.ts
@@ -0,0 +1,141 @@
+import "express-async-errors"
+import request from 'supertest';
+import { PrismaClient } from '@prisma/client';
+import express from 'express';
+import empresasController from "../src/controllers/empresasController";
+import catalogoControler from "../src/controllers/catalogosController";
+import produtoController from "../src/controllers/produtosController";
+import { errorMiddleware } from "../src/middlewares/error";
+import { EmpresaGetDeleteRequestDto } from "../src/dto/empresaDto/empresaGetDeleteRequestDto";
+import { EmpresaContatoPatchRequestDto } from "../src/dto/empresaDto/empresaContatoPatchRequestDto";
+import { EmpresaPostPutRequestDto } from "../src/dto/empresaDto/empresaPostPutRequestDto";
+
+const app = express();
+app.use(express.json());
+app.use('/empresas', empresasController);
+app.use('/catalogos', catalogoControler);
+app.use('/produtos', produtoController);
+app.use(errorMiddleware)
+const server = app.listen(process.env.PORT, () => {
+	console.log(`Servidor rodando em http://localhost:${process.env.PORT}`);
+	console.log(`url = env(${process.env.DATABASE_URL})`)
+});
+
+const prisma = new PrismaClient();
+
+beforeAll(async () => {
+	prisma.$connect();
+});
+
+beforeEach(async () => {
+	await prisma.$executeRaw`DELETE FROM empresas;`
+	await prisma.$executeRaw`DELETE FROM catalogos;`
+	await prisma.$executeRaw`DELETE FROM produtos;`
+	await prisma.$executeRaw`DELETE FROM sqlite_sequence WHERE name='empresas';`
+	await prisma.$executeRaw`DELETE FROM sqlite_sequence WHERE name='catalogos';`
+	await prisma.$executeRaw`DELETE FROM sqlite_sequence WHERE name='produtos';`
+	const empresaPostPutRequestDto: EmpresaPostPutRequestDto = {
+		"nome": "nome fantasia",
+		"endereco": "endereco fantasia",
+		"telefone": "contato fantasia",
+		"cnpj": "cnpj fantasia",
+		"email": "email fantasia",
+		"username": "username fantasia",
+		"cep": "cep fantasia",
+		"senha": "fantasia"
+	}
+	await prisma.empresas.create({
+		data: {
+			...empresaPostPutRequestDto
+		}
+	});
+});
+
+afterEach(async () => {
+	await prisma.$executeRaw`DELETE FROM empresas;`
+	await prisma.$executeRaw`DELETE FROM catalogos;`
+	await prisma.$executeRaw`DELETE FROM produtos;`
+	await prisma.$executeRaw`DELETE FROM sqlite_sequence WHERE name='empresas';`
+	await prisma.$executeRaw`DELETE FROM sqlite_sequence WHERE name='catalogos';`
+	await prisma.$executeRaw`DELETE FROM sqlite_sequence WHERE name='produtos';`
+});
+
+afterAll(async () => {
+	prisma.$disconnect();
+	server.close()
+});
+
+describe('GET /empresas/pesquisar', () => {
+	it('Pesquisa um empresa que nao existe', async () => {
+		const empresaGetDeleteRequestDto: EmpresaGetDeleteRequestDto = {
+			'id': 100
+		}
+		const response = await request(app).get('/empresas/pesquisar').send(empresaGetDeleteRequestDto);
+
+		expect(response.status).toBe(404);
+		expect(response.body).toEqual({ message: 'Empresa não exsite!' });
+	});
+	it('Pesquisa um empresa que existe', async () => {
+		const empresaGetDeleteRequestDto: EmpresaGetDeleteRequestDto = {
+			'id': 1
+		}
+		const response = await request(app).get('/empresas/pesquisar').send(empresaGetDeleteRequestDto);
+
+		expect(response.status).toBe(200);
+		expect(response.body).toHaveProperty('cnpj', 'cnpj fantasia');
+	});
+});
+
+describe('DELETE /empresas/remover', () => {
+	it('Remove uma empresa que nao existe', async () => {
+		const empresaGetDeleteRequestDto: EmpresaGetDeleteRequestDto = {
+			'id': 100
+		}
+		const response = await request(app).delete('/empresas/pesquisar').send(empresaGetDeleteRequestDto);
+
+		expect(response.status).toBe(404);
+		expect(response.body).toEqual({});
+	});
+});
+
+describe('PATCH /empresas/atualizar/contato', () => {
+	it('Atualiza uma empresa que não existe', async () => {
+		const empresaContatoPatchRequestDto: EmpresaContatoPatchRequestDto = {
+			'id': 100,
+			'telefone': 'telefone novo'
+		}
+		const response = await request(app).patch('/empresas/atualizar/contato').send(empresaContatoPatchRequestDto);
+
+		expect(response.status).toBe(404);
+		expect(response.body).toEqual({ message: 'Empresa não exsite!' });
+	});
+	it('Atualiza uma empresa que existe', async () => {
+		const empresaContatoPatchRequestDto: EmpresaContatoPatchRequestDto = {
+			'id': 1,
+			'telefone': 'telefone novo'
+		}
+		const response = await request(app).patch('/empresas/atualizar/contato').send(empresaContatoPatchRequestDto);
+
+		expect(response.status).toBe(201);
+		expect(response.body).toHaveProperty('telefone', 'telefone novo');
+	});
+});
+
+describe('POST /empresas/adicionar', () => {
+	it('Adiciona uma empresa', async () => {
+		const empresaPostPutRequestDto: EmpresaPostPutRequestDto = {
+			"nome": "nome string",
+			"endereco": "endereco string",
+			"telefone": "contato string",
+			"cnpj": "cnpj string",
+			"email": "email string",
+			"username": "username string",
+			"cep": "cep string",
+			"senha": "123"
+		}
+		const response = await request(app).post('/empresas/adicionar').send(empresaPostPutRequestDto);
+
+		expect(response.status).toBe(201);
+		expect(response.body).toHaveProperty('id');
+	});
+});
