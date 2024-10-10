211b357397de317abc0f2dc0a34fcc22ac1efd92
Fabio Vinicius <fabiovinicius1@outlook.com>
2024-10-10 15:28:33 -0300

    testes para autenticaçao de empresa

---
+++ tests/auth.spec.ts
@@ -0,0 +1,65 @@
+import request from 'supertest';
+import Jwt from 'jsonwebtoken';
+import { app, server } from '../src/index';
+import { PrismaClient } from '@prisma/client';
+import { EmpresaPostPutRequestDto } from '../src/dto/empresaDto/empresaPostPutRequestDto';
+import { EmpresaLoginRequestDTO } from '../src/dto/empresaDto/EmpresaLoginRequestDTO';
+
+const prisma = new PrismaClient();
+
+beforeEach(async () => {
+	await prisma.$executeRaw`TRUNCATE TABLE "Empresas" RESTART IDENTITY CASCADE;`;
+
+	const empresaPostPutRequestDto: EmpresaPostPutRequestDto = {
+		"nome": "nome fantasia",
+		"endereco": "endereco fantasia",
+		"telefone": "contato fantasia",
+		"cnpj": "cnpj fantasia",
+		"email": "email fantasia",
+		"username": "username fantasia",
+		"cep": "cep fantasia",
+		"senha": "fantasia"
+	}
+	await request(app).post('/empresas/adicionar').send(empresaPostPutRequestDto);
+
+});
+
+afterEach(async () => {
+	await prisma.$executeRaw`TRUNCATE TABLE "Empresas" RESTART IDENTITY CASCADE;`;
+});
+
+afterAll(async () => {
+	prisma.$disconnect();
+	server.close()
+});
+
+describe('POST /auth/login/empresa', () => {
+	it('Login realizado com sucesso', async () => {
+		const empresaLoginRequestDTO: EmpresaLoginRequestDTO = {
+			'username': 'username fantasia',
+			'senha': 'fantasia'
+		}
+		const response = await request(app).post('/auth/login/empresa').send(empresaLoginRequestDTO);
+		expect(response.status).toBe(201);
+		const decoded = Jwt.verify(response.body, process.env.SECRET!);
+		expect(decoded).toEqual(expect.objectContaining({ username: 'username fantasia', id: 1 }));
+	});
+	it('Login passado incorreto', async () => {
+		const empresaLoginRequestDTO: EmpresaLoginRequestDTO = {
+			'username': 'username fant',
+			'senha': 'fantasia'
+		}
+		const response = await request(app).post('/auth/login/empresa').send(empresaLoginRequestDTO);
+		expect(response.status).toBe(401);
+		expect(response.body).toEqual({ message: 'Login ou senha incorretos!' });
+	});
+	it('Senha passado incorreto', async () => {
+		const empresaLoginRequestDTO: EmpresaLoginRequestDTO = {
+			'username': 'username fantasia',
+			'senha': 'fant'
+		}
+		const response = await request(app).post('/auth/login/empresa').send(empresaLoginRequestDTO);
+		expect(response.status).toBe(401);
+		expect(response.body).toEqual({ message: 'Login ou senha incorretos!' });
+	});
+});
