83fb8ac430b30cfefb3da0bc2a4974975715d4cd
GuilhermeFernandesCC <guilherme.fernandes@ccc.ufcg.edu.br>
2024-12-08 11:01:18 -0300

    Update: produto controllers

--- src/controllers/produtosController.ts
+++ src/controllers/produtosController.ts
@@ -5,33 +5,44 @@ import {getProdutoService} from '../services/produtoService/getProdutoService';
 import { getAllProdutoService } from '../services/produtoService/getAllProdutoService';
 import { getByTagProdutoService } from '../services/produtoService/getByTagProdutoService';
 import { atualizarProdutoService } from '../services/produtoService/atualizarProdutoService';
-import { getBycatalogoIdProdutoService } from '../services/produtoService/getBycatalogoIdProdutoService';
+import { getBycatalogoIdProdutoService, getByEnterpriseIdProdutoService } from '../services/produtoService/getByEnterpriseIdProdutoService ';
+import { getByEnterpriseAndByTagProdutoRepository } from '../repositories/produtoRepository';
 const router = Router();
-
+//Add produto
 router.post('/adicionar', async (req: Request, res: Response) => {
 	const produtoPostPutRequestDto: ProdutoPostPutRequestDto = req.body;
 	const result = await adicionarProdutoService(produtoPostPutRequestDto);
 	return res.status(201).json(result);
 });
+//get produto por id
 router.get('/get/:id',async (req:Request,res:Response) => {
 	const result = await getProdutoService(Number(req.params.id));
 	return res.status(200).json(result);
 });
+//get all produtos
 router.get('/get',async (req:Request,res:Response) => {
 	const result = await getAllProdutoService();
 	return res.status(200).json(result)
 })
+//update
 router.post('/atualizar/:id',async (req:Request, res:Response)=>{
 	const produtoPostPutRequestDto: ProdutoPostPutRequestDto = req.body;
 	const result = await atualizarProdutoService(Number(req.params.id),produtoPostPutRequestDto)
 	return res.status(200).json(result)
 })
-router.get('/get',async (req:Request,res:Response)=>{
-	const result = await getByTagProdutoService(req.body);
+// get by tag id 
+router.get('/getByTag/:id',async (req:Request,res:Response)=>{
+	const result = await getByTagProdutoService(Number(req.params.id));
 	return res.status(200).json(result)
 })
-router.get('/get/:catalogoId',async(req:Request,res:Response)=>{
-	const resutl = await getBycatalogoIdProdutoService(Number(req.params.id));
-	return res.status(200).json(resutl)
+// get many by enterprise id
+router.get('/getByEnterprise/:id',async(req:Request,res:Response)=>{
+	const result = await getByEnterpriseIdProdutoService(Number(req.params.id));
+	return res.status(200).json(result)
+})
+// get many vy enterprise id and tag id
+router.get('/getByEnterprise/:id/byTag/:tagId',async(req:Request,res:Response)=>{
+	const result = await getByEnterpriseAndByTagProdutoRepository(Number(req.params.id),Number(req.params.tagId));
+	return res.status(200).json(result)
 })
 export default router;
